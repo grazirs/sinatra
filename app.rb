d530f51927b80c4f9e89e2ac460d5ca3facb3a07
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-01-26 18:13:21 -0300

    add routes to app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 
 get '/' do
-  'Hello world'
+  send_file File.join(settings.public_folder, 'index.html')
+end
+
+get '/pullrequest' do
+  send_file File.join(settings.public_folder, 'pr.html')
+end
+
+get '/university' do
+  send_file File.join(settings.public_folder, 'university.html')
 end
